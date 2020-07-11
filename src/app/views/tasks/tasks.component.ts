7f59c6bca19ab7ad4023747cb85b1b4cd25ac637
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-11 23:55:50 +0500

    Добавлена пагинация, сортировка

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,17 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Task} from '../../model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
   // поля для таблицы( должны совпадать с названиями переменных класса)
   public displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
   public dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+  // ссылки и компоенты таблицы
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
+
   tasks: Task[];
   constructor(private dataHandler: DataHandlerService) { }
 
@@ -21,6 +28,10 @@ export class TasksComponent implements OnInit {
     // dataSource Обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы)
     this.dataSource = new MatTableDataSource();
     this.refreshTable();
+  }
+
+  ngAfterViewInit(): void {
+    this.addTableObject();
   }
 
   toogleTaskCompleted(task: Task): void {
@@ -41,7 +52,35 @@ export class TasksComponent implements OnInit {
     return '#fff';
   }
 
+  // показывает задачи с применением всех текущих условий (категория, поиск, фильтры и пр.)
   private refreshTable(): void {
-    this.dataSource.data = this.tasks;
+    this.dataSource.data = this.tasks; // обновить источник данных ( т.к. данные массивы tasks обновились)
+
+    this.addTableObject();
+
+    // когда получаем новые данные...
+    // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      // по каким полям выполнять сортировку для каждого столбца
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? !task.date : null;
+        }
+         case 'title': {
+          return task.title;
+        }
+      }
+    };
+  }
+
+  private addTableObject(): void {
+    this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+    this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол. записей, страниц)
   }
 }
