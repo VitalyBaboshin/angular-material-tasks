397599d0641ad22059c9ecdbdf27c447d3b52228
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-13 22:35:44 +0500

    Добавлен компонет для диалогового окна, изменение категории. с возможностью из этого окна удаления категории и вызова окна подтверждения удаления

---
+++ src/app/dialog/edit-category/edit-category.component.ts
@@ -0,0 +1,54 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {Category} from '../../model/Category';
+import {ConfirmDialogComponent} from '../confirm-dialog/confirm-dialog.component';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styleUrls: ['./edit-category.component.scss']
+})
+export class EditCategoryComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<Category>,
+    @Inject(MAT_DIALOG_DATA) private data: [Category, string],
+    private dialog: MatDialog
+  ) { }
+
+  public dialogTitle: string;
+  public category: Category;
+  public tmpTitle: string;
+
+  ngOnInit(): void {
+    this.category = this.data[0];
+    this.dialogTitle = this.data[1];
+    this.tmpTitle = this.category.title;
+  }
+
+  public onConfirm(): void {
+    this.category.title = this.tmpTitle;
+    this.dialogRef.close(this.category);
+  }
+
+  public onCancel(): void {
+    this.dialogRef.close(null);
+  }
+
+  public delete(): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '500px',
+      data: {
+        dialogTitle: 'Подтвердите действие',
+        message: `Вы действительно хотите удалить категорию: "${this.category.title}"? (сами задачи не удалятся)`
+      },
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe( result => {
+      if (result) {
+        this.dialogRef.close('delete');
+      }
+    });
+  }
+}
