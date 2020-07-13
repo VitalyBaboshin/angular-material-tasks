a45da0a1816a6142eb19910ab2b9eab28d94aca5
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-13 22:32:20 +0500

    Реализован метод удаления категории, с каскадным удаление категории в задачах Реализован метод update

--- src/app/data/impl/categoryDaoArray.ts
+++ src/app/data/impl/categoryDaoArray.ts
@@ -17,14 +17,23 @@ export class CategoryDaoArray implements CategoryDao{
   }
 
   delete(id: number): Observable<Category> {
-    return undefined;
+    TestData.tasks.forEach(task => {
+      if (task.category && task.category.id === id) {
+        task.category = null;
+      }
+    });
+    const tmpCategory = TestData.categories.find(t => t.id === id);
+    TestData.categories.splice(TestData.categories.indexOf(tmpCategory), 1);
+    return of(tmpCategory);
   }
 
   search(title: string): Observable<Category[]> {
     return undefined;
   }
 
-  update(T): Observable<Category> {
-    return undefined;
+  update(category: Category): Observable<Category> {
+    const tmpCategory = TestData.categories.find(t => t.id === category.id);
+    TestData.categories.splice(category.id - 1, 1, category);
+    return of(tmpCategory);
   }
 }
