437bf0fb105d9ad93ffe6dbabf5445ae08e1261e
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-16 23:19:07 +0500

    В импорты добавлен EditPriorityDialogComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import {StatCardComponent} from './views/stat/stat-card/stat-card.component';
 import {ColorPickerModule} from 'ngx-color-picker';
 import {PrioritiesComponent} from './views/priorities/priorities.component';
 import {SettingsDialogComponent} from './dialog/settings-dialog/settings-dialog.component';
+import {EditPriorityDialogComponent} from './dialog/edit-priority-dialog/edit-priority-dialog.component';
 
 registerLocaleData(localeRu);
 @NgModule({
@@ -50,7 +51,8 @@ registerLocaleData(localeRu);
     StatComponent,
     StatCardComponent,
     PrioritiesComponent,
-    SettingsDialogComponent
+    SettingsDialogComponent,
+    EditPriorityDialogComponent
   ],
   imports: [
     MatTableModule,
