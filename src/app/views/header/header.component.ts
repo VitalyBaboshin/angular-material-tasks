4332249315c4a77a136a1791af09e120bea6a1cc
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-19 00:16:04 +0500

    Добавлено исходящее событие в в родительский компонет (показать / скрыть меню)

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -21,6 +21,9 @@ export class HeaderComponent implements OnInit {
   @Output()
   toggleStat = new EventEmitter<boolean>();
 
+  @Output()
+  toggleMenu = new EventEmitter(); // показать/скрыть меню
+
   constructor(
     private dialog: MatDialog,
     private introService: IntroService
@@ -44,5 +47,9 @@ export class HeaderComponent implements OnInit {
 
   showIntro(): void {
     this.introService.startIntroJS(false);
+  }
+
+  onToggleMenu(): void {
+    this.toggleMenu.emit(); // показать/скрыть меню
   }
 }
