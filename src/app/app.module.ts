25bfde03aea940b94d22a6ee5353c202ceb3ddcf
VitaliyBaboshin <vitaliy2@ro.ru>
2020-07-19 16:45:56 +0500

    В импорт добавлен DeviceDetectorModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import {PrioritiesComponent} from './views/priorities/priorities.component';
 import {SettingsDialogComponent} from './dialog/settings-dialog/settings-dialog.component';
 import {EditPriorityDialogComponent} from './dialog/edit-priority-dialog/edit-priority-dialog.component';
 import {SidebarModule} from 'ng-sidebar';
+import {DeviceDetectorModule} from 'ngx-device-detector';
 
 registerLocaleData(localeRu);
 @NgModule({
@@ -74,7 +75,8 @@ registerLocaleData(localeRu);
     MatNativeDateModule,
     MatCheckboxModule,
     ColorPickerModule,
-    SidebarModule
+    SidebarModule,
+    DeviceDetectorModule.forRoot()
   ],
   providers: [],
   entryComponents: [
